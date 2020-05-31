fd5f6c0c3c0c8fd0f1391f7d31e5d99b8900f611
Igor Tuag <igortuag@gmail.com>
2020-05-31 14:23:10 -0300

    Create users on backend

--- frontend/src/components/user/userCrud.jsx
+++ frontend/src/components/user/userCrud.jsx
@@ -21,7 +21,7 @@ export default class UserCrud extends Component {
     clear() {
         this.setState({ user: initialState.user })
     }
-    
+
     save() {
         const user = this.state.user
         const method = user.id ? 'put' : 'post'
@@ -38,11 +38,63 @@ export default class UserCrud extends Component {
     getUpdatedList(user) {
         const list = this.state.list.filter(u => u.id !== user.id)
     }
+
+    updateField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState({ user })
+    }
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Name</label>
+                            <input type="text" className="form-control" 
+                                name="name"
+                                value={this.state.user.name}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Enter the name..." />
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" className="form-control" 
+                                name="email"
+                                value={this.state.user.email}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Enter the E-mail..." />
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary"
+                            onClick={e => this.save(e)}>
+                            Save
+                        </button>
+
+                        <button className="btn btn-secondary ml-2"
+                            onClick={e => this.clear(e)}>
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
  
     render() {
         return (
             <Main {...headerProps}>
-                User Registration
+                {this.renderForm()}
             </Main>
         )
     }
