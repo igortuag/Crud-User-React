0ab1876f5833dc6b4ea558fe0c6e4eb5e21ad773
Igor Tuag <igortuag@gmail.com>
2020-05-31 11:54:50 -0300

    Content Component

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props =>
+    <Main icon="home" title="Home"
+        subtitile="Simples Crud using React + Json Server">
+        <div className="display-4">Wellcome!</div>
+        <hr />
+        <p className="mb-0">System to exemplify the construction of a register developed in React!</p>
+    </Main>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -5,13 +5,13 @@ import React from 'react'
 
 import Logo from '../components/templates/Logo'
 import Nav from '../components/templates/Nav'
-import Main from '../components/templates/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/templates/Footer'
 
 export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main icon="home" title="Home" subtitile="Simples Crud using React + Json Server"/>
+        <Home />
         <Footer />
     </div>
